6b57c954a00fcc2a747e5a468f069317249b7c2a
AntonioCammarata <antoniocamm2001@gmail.com>
2023-04-27 10:49:22 +0200

    ExerciseDone

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ import ConditionalRendering from "./ConditionalRendering";
 import ConditionalRenderingIf from "./Conditional Rendering - If prop is present";
 import ConditionalRenderingLess from "./Conditional Rendering - If less than `65`";
 import { Young } from "./Conditional Rendering - Render the You are very young";
+import { Counter } from "./Counter";
 
 function App() {
   return (
     <>
-      <Young age={19} />
+      <Counter />
     </>
   );
 }
