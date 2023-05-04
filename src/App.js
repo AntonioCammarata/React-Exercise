683da8c61c293ecfc49c9c2ef3b26b1c0caff2de
AntonioCammarata <antoniocamm2001@gmail.com>
2023-05-04 14:14:12 +0200

    ExerciseDone

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import ConditionalRendering from "./ConditionalRendering";
 import ConditionalRenderingIf from "./Conditional Rendering - If prop is present";
 import ConditionalRenderingLess from "./Conditional Rendering - If less than `65`";
 import { Young } from "./Conditional Rendering - Render the You are very young";
-import { Counter } from "./Counter";
+import { Counter1 } from "./Counter";
 import { CounterProps } from "./CounterProps";
+import { CounterProps1 } from "./CounterEsercizio2";
 
 function App() {
   return (
     <>
       <Welcome name={<strong>"Antonio"</strong>} />
+      <CounterProps1 initialValue={1} incrementBy={2} />
     </>
   );
 }
