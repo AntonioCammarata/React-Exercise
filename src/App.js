614f184c4258603a5590ef74257ddf9f33e4d0ef
AntonioCammarata <antoniocamm2001@gmail.com>
2023-05-04 13:31:42 +0200

    ExerciseDone

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Call } from "./call";
 import { Sum } from "./sum";
 import { Welcome } from "./welcome";
 import { WelcomeAge } from "./welcome-2.";
-import { Age } from "./Props-Extract message";
+import { Age } from "./age";
 import ConditionalRendering from "./ConditionalRendering";
 import ConditionalRenderingIf from "./Conditional Rendering - If prop is present";
 import ConditionalRenderingLess from "./Conditional Rendering - If less than `65`";
