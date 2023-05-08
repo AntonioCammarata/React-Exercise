0871750fca77b43d882090cbc8d7579b1aa23986
AntonioCammarata <antoniocamm2001@gmail.com>
2023-05-08 11:52:13 +0200

    ExerciseDone

---
+++ src/FocusComponent.jsx
@@ -0,0 +1,52 @@
+import React, { useRef } from "react";
+import { useState } from "react";
+
+function UncontrolledLoginFocus(onlogin) {
+  const UsernameImputRef = useRef(null);
+  const passwordImputRef = useRef(null);
+  const rememberImputRef = useRef(null);
+
+  const handleSubmit = () => {
+    const username = UsernameImputRef.current.value;
+    const password = passwordImputRef.current.value;
+    const remember = rememberImputRef.current.checked;
+    console.log(username, password, remember);
+    onlogin({ UsernameImputRef, passwordImputRef, rememberImputRef });
+  };
+
+  const [foucsText, setFocusText] = useState(true);
+  function handleInput(event) {
+    let value = event.target.value;
+    setFocusText(value);
+  }
+
+  const isDisabled = !UsernameImputRef || !passwordImputRef;
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          autoFocus
+          onInput={handleInput}
+          value={foucsText}
+          id="username"
+          ref={UsernameImputRef}
+        />
+      </div>
+      
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" ref={passwordImputRef} />
+      </div>
+      <div>
+        <label htmlFor="remember">Remember</label>
+        <input type="checkbox" id="remember" ref={rememberImputRef} />
+      </div>
+      <button type="submit" disabled={isDisabled}>
+        Log in
+      </button>
+    </form>
+  );
+}
+export default UncontrolledLoginFocus;
