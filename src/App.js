cc094b563d9de0255c07ccea78db38f1ab59fe06
AntonioCammarata <antoniocamm2001@gmail.com>
2023-05-11 17:07:17 +0200

    ExerciseDone

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import { Hello } from "./Hello";
-import { Call } from "./call";
-import { Sum } from "./sum";
-import { Welcome } from "./welcome";
-import { WelcomeAge } from "./welcome-2.";
-import { Age } from "./age";
-import ConditionalRendering from "./ConditionalRendering";
-import ConditionalRenderingIf from "./Conditional Rendering - If prop is present";
-import ConditionalRenderingLess from "./Conditional Rendering - If less than `65`";
-import { Young } from "./Conditional Rendering - Render the You are very young";
-import { Counter1 } from "./Counter";
-import { CounterProps } from "./CounterProps";
-import { CounterProps1 } from "./CounterEsercizio2";
-import CounterClick from "./CounterButton";
-import ClickTracker from "./ClickTraker";
-import InteractiveWelcome from "./FormInteractive";
-import Login from "./login";
-import UncontrolledLogin from "./UncontrolledLogin";
-import TodoList from "./ToDo";
-import TodoList2 from "./ToDo2";
-import TodoList3 from "./Todo3";
-import TodoList4 from "./ToDo4";
 import { Languagecontext } from "./Language/CreateContext";
 import { useState } from "react";
+import DisplayLanguage from "./Language/DisplayLanguage";
+import ClickCounterEffect from "./Components/SideEffect";
+import GithubUserList from "./Components/GitHubUserList";
+import CounterEffect from "./Components/SideEffect2";
+import { GithubUser } from "./Components/GitHubUser";
 
 function App() {
   const [language, setLanguage] = useState("it");
@@ -37,6 +20,12 @@ function App() {
       <Languagecontext.Provider value={language}>
         <DisplayLanguage />
       </Languagecontext.Provider>
+      <GithubUser />
+      <ClickCounterEffect
+        onCounterChange={(count) => {
+          console.log("The counter value :", count);
+        }}
+      />
     </>
   );
 }
